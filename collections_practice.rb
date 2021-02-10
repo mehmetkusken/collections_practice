67f897f1df719f9c06d2d48acce9a4c606f857ee
Mehmet <mehmetkusken@gmail.com>
2021-02-09 22:05:57 -0700

    commit tree 811f909018c41d601c173b1444dbba9f28672a7d

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,36 @@
+def sort_array_asc(array)
+    array.sort
+end
+def sort_array_desc(array)
+    array.sort.reverse
+end
+def sort_array_char_count(array)
+    array.sort {|a,b| a.length <=> b.length}
+       
+end
+def swap_elements(array)
+    array[1],array[2]=array[2],array[1]
+    return array
+end
+def reverse_array(array)
+    array.reverse.each {|a| a}
+end
+def kesha_maker(array)
+    array.each {|element| element[2] = "$"}
+end
+def find_a(array)
+    array.select {|element| element[0]== "a"}
+end
+def sum_array(array)
+    array.inject {|add,num| add+num}
+end
+def add_s(array)
+    array.collect.with_index do |element , index|
+        if index ==1
+            element = element
+        else 
+            element = element + "s"
+        end
+    end
+end
 
